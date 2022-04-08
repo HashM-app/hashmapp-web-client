8e721607d9282e3a9d6a1c6e89f5ce848f7e45ec
Z. Charles Dziura <zachary.dziura@csquaredsystems.com>
2022-04-08 11:09:47 -0400

    Create files page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FilesComponent } from './pages/files/files.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'files', component: FilesComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
+	declarations: [FilesComponent],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
