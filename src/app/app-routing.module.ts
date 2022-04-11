860b8a3842d4916a3704a7dfbcda85be1a48ddda
Z. Charles Dziura <zachary.dziura@csquaredsystems.com>
2022-04-11 10:15:06 -0400

    Add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from './material.module';
 import { FilesComponent } from './pages/files/files.component';
 import { PipesModule } from './pipes/pipes-module.module';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [{ path: 'files', component: FilesComponent }];
 
 @NgModule({
 	imports: [CommonModule, MaterialModule, PipesModule, RouterModule.forRoot(routes)],
 	exports: [MaterialModule, RouterModule],
-	declarations: [FilesComponent],
+	declarations: [FilesComponent, LoginComponent],
 })
 export class AppRoutingModule {}
