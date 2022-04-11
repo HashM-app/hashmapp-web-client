9ef570887bb94803e1fb5639bee53c374af931cb
Z. Charles Dziura <zachary.dziura@csquaredsystems.com>
2022-04-11 10:27:53 -0400

    Add additional material design module dependencies

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-const MODULES = [MatIconModule, MatTableModule, MatToolbarModule];
+const MODULES = [
+	MatButtonModule,
+	MatCardModule,
+	MatDividerModule,
+	MatFormFieldModule,
+	MatIconModule,
+	MatTableModule,
+	MatToolbarModule,
+	MatInputModule,
+];
 @NgModule({
 	declarations: [],
 	imports: MODULES,
