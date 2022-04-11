edc9d9eefa5ede736b03f9ac79f6c097ba5f5ddf
Z. Charles Dziura <zachary.dziura@csquaredsystems.com>
2022-04-11 12:48:28 -0400

    Add reactive forms module dependency

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+
 import { MaterialModule } from './material.module';
 import { FilesComponent } from './pages/files/files.component';
-import { PipesModule } from './pipes/pipes-module.module';
 import { LoginComponent } from './pages/login/login.component';
+import { PipesModule } from './pipes/pipes-module.module';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
@@ -13,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [CommonModule, MaterialModule, PipesModule, RouterModule.forRoot(routes)],
+	imports: [CommonModule, MaterialModule, ReactiveFormsModule, PipesModule, RouterModule.forRoot(routes)],
 	exports: [MaterialModule, RouterModule],
 	declarations: [FilesComponent, LoginComponent],
 })
